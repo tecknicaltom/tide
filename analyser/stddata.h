80012c2c4b5a62c6f074343e2be7d8379db91cf8
seppel <seppel>
2002-06-30 16:57:12 +0000

    all changes since branch

--- analyser/stddata.h
+++ analyser/stddata.h
@@ -2,7 +2,7 @@
  *	HT Editor
  *	stddata.h
  *
- *	Copyright (C) 1999, 2000, 2001 Sebastian Biallas (sb@web-productions.de)
+ *	Copyright (C) 1999-2002 Sebastian Biallas (sb@web-productions.de)
  *
  *	This program is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License version 2 as
@@ -18,81 +18,17 @@
  *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#ifndef stddata_h
-#define stddata_h
+#ifndef STDDATA_H
+#define STDDATA_H
 
-#include <stdio.h>
 #include "common.h"
-
-/*
-class tdata {
-public:
-				tdata() {};
-				~tdata() {};
-	void			init();
-	void			load(FILE *f);
-	void	virtual	done();
-	void virtual	store();
-};
-*/
-
-
-struct queueitem {
-	queueitem	*next;
-	void			*data;
-};
-
-class queue: public object {
-public:
-	queueitem			*first, *last;
-					queue() {};
-	virtual	~queue() {};
-			void		init();
-			int		load(ht_object_stream *f);
-	virtual	int 		load_element(ht_object_stream *f, void **data);
-	virtual	void		done();
-			void		clear();
-			int		count();
-			bool		empty();
-			void		enqueue(void *p);
-			void 	*dequeue();
-	virtual	void 	free_element(void *data);
-	virtual	void		store(ht_object_stream *f);
-	virtual	void		store_element(ht_object_stream *f, void *data);
-			void		*top();
-};
-
-struct tstackitem {
-	tstackitem	*next;
-	void			*data;
-};
-
-class tstack {
-public:
-	tstackitem		*stop;
-					tstack() {};
-	virtual			~tstack() {};
-			void		init();
-			int		load(ht_object_stream *f);
-	virtual   int 		load_element(ht_object_stream *f, void **data);
-	virtual	void		done();
-			void		clear();
-			int		count();
-			bool		empty();
-	virtual 	void 	free_element(void *data);
-			void		*top();
-			void		push(void *p);
-			void		*pop();
-	virtual	void		store(ht_stream *f);
-	virtual	void		store_element(ht_object_stream *f, void *data);
-};
 
 struct area_s {
 	area_s	*left, *right;
-	dword	start, end;
+	Object	*start, *end;
 };
 
-class area: public object {
+class Area: public Object {
 public:
 	area_s			*a;
 
@@ -101,19 +37,18 @@ public:
 	virtual	void 	done();
 			OBJECT_ID	object_id();
 
-			void		add(dword Start, dword End);
-			bool		contains(dword V);
-			area_s	*get_area(dword at);
-			dword	find_next(dword From);
-			dword	find_prev(dword From);
-			void		free_recursive(area_s *p);
+			void		add(Object *Start, Object *End);
+			bool		contains(Object *V);
+			area_s	*getArea(Object *at);
+			Object	*findNext(Object *From);
+			Object	*findPrev(Object *From);
+			void		freeRecursive(area_s *p);
 	virtual	void		store(ht_object_stream *f);
-#ifdef DEBUG
+#ifdef DEBUG_FIXNEW
 			void		dump();
 #endif
 };
 
-#define ATOM_QUEUE MAGICD("UEUE")
 #define ATOM_AREA MAGICD("AREA")
 
 bool init_stddata();
