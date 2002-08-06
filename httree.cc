f8d790bac9595add34d4b859fef775a0479391be
steveman <steveman>
2002-08-06 19:50:39 +0000

    update to 0.6.0c

--- httree.cc
+++ httree.cc
@@ -380,7 +380,7 @@ void ht_static_treeview::done()
 	ht_treeview::done();
 }
 
-void *ht_static_treeview::add_child(void *node, char *text, ht_data *Data=NULL)
+void *ht_static_treeview::add_child(void *node, char *text, ht_data *Data)
 {
 	if (node) {
 		return add_node(&((static_node *)node)->child, text, Data);
@@ -394,7 +394,7 @@ void *ht_static_treeview::add_child(void *node, char *text, ht_data *Data=NULL)
 	}
 }
 
-void	*ht_static_treeview::add_node(static_node **node, char *text, ht_data *Data=NULL)
+void	*ht_static_treeview::add_node(static_node **node, char *text, ht_data *Data)
 {
 	static_node **p = node;
 	static_node *prev = NULL;
@@ -413,7 +413,7 @@ void	ht_static_treeview::adjust(void *node, bool expand)
 	((static_node *)node)->expanded = expand;
 }
 
-static_node *ht_static_treeview::create_node(char *text, static_node *prev, ht_data *Data=NULL)
+static_node *ht_static_treeview::create_node(char *text, static_node *prev, ht_data *Data)
 {
 	static_node *node = (static_node *)malloc(sizeof(static_node));
 	node->text = ht_strdup(text);
