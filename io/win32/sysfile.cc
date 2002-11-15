802a688e15fdba00ac99dd576313bb6396e572f5
steveman <steveman>
2002-11-15 12:23:47 +0000

    .

---
+++ io/win32/sysfile.cc
@@ -0,0 +1,194 @@
+/* 
+ *	HT Editor
+ *	sysfile.cc - file system functions for Win32
+ *
+ *	Copyright (C) 1999-2002 Sebastian Biallas (sb@web-productions.de)
+ *	Copyright (C) 1999-2002 Stefan Weyergraf (stefan@weyergraf.de)
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License version 2 as
+ *	published by the Free Software Foundation.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#include "../sys.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+
+struct winfindstate {
+	HANDLE fhandle;
+	WIN32_FIND_DATA find_data;
+};
+
+int sys_canonicalize(char *result, const char *filename)
+{
+	char *dunno;
+	return (GetFullPathName(filename, HT_NAME_MAX, result, &dunno) > 0) ? 0 : ENOENT;
+}
+
+uint filetime_to_ctime(FILETIME f)
+{
+	uint64 q;
+	QWORD_SET_LO(q, f.dwLowDateTime);
+	QWORD_SET_HI(q, f.dwHighDateTime);
+	q = q / mkuint64(10000000);		// 100 nano-sec to full sec
+	return QWORD_GET_LO(q) + 1240431886;	// MAGIC: this is 1.1.1970 minus 1.1.1601 in seconds
+}
+
+void sys_findfill(pfind_t *pfind)
+{
+	/*DWORD dwFileAttributes;
+	FILETIME ftCreationTime;
+	FILETIME ftLastAccessTime;
+	FILETIME ftLastWriteTime;
+	DWORD    nFileSizeHigh;
+	DWORD    nFileSizeLow;
+	DWORD    dwReserved0;
+	DWORD    dwReserved1;
+	TCHAR    cFileName[ MAX_PATH ];
+	TCHAR    cAlternateFileName[ 14 ];*/
+	winfindstate *wfs=(winfindstate*)pfind->findstate;
+	pfind->name = (char *)&wfs->find_data.cFileName;
+	pfind->stat.caps = pstat_ctime|pstat_mtime|pstat_atime|pstat_size|pstat_mode_type;
+
+	pfind->stat.size = wfs->find_data.nFileSizeLow | (mkuint64(wfs->find_data.nFileSizeHigh) << 32);
+
+	pfind->stat.mode = (wfs->find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) ? HT_S_IFDIR : HT_S_IFREG;
+	pfind->stat.ctime = filetime_to_ctime(wfs->find_data.ftCreationTime);
+	pfind->stat.mtime = filetime_to_ctime(wfs->find_data.ftLastWriteTime);
+	pfind->stat.atime = filetime_to_ctime(wfs->find_data.ftLastAccessTime);
+}
+
+int sys_findfirst(const char *dirname, pfind_t *pfind)
+{
+	char *Dirname = (char *)malloc(strlen(dirname)+5);
+	strcpy(Dirname, dirname);
+	int dnl=strlen(dirname);
+	if ((dirname[dnl-1]!='\\') && (dirname[dnl-1]!='/')) {
+		Dirname[dnl]='\\';
+		Dirname[dnl+1]=0;
+	}
+	char *s=Dirname;
+	while ((s=strchr(s, '/'))) *s='\\';
+	strcat(Dirname, "*.*");
+
+	pfind->findstate=malloc(sizeof (winfindstate));
+	winfindstate *wfs=(winfindstate*)pfind->findstate;
+
+	wfs->fhandle = FindFirstFile(Dirname, &wfs->find_data);
+	free(Dirname);
+	if (wfs->fhandle == INVALID_HANDLE_VALUE) {
+		free(pfind->findstate);
+		return ENOENT;
+	}
+	sys_findfill(pfind);
+	return 0;
+}
+
+int sys_findnext(pfind_t *pfind)
+{
+	winfindstate *wfs=(winfindstate*)pfind->findstate;
+	
+	if (!FindNextFile(wfs->fhandle, &wfs->find_data)) {
+		return ENOENT;
+	}
+	sys_findfill(pfind);
+	return 0;
+}
+
+int sys_findclose(pfind_t *pfind)
+{
+	int r=FindClose(((winfindstate*)pfind->findstate)->fhandle);
+	free(pfind->findstate);
+	return r ? ENOENT : 0;
+}
+
+int sys_pstat(pstat_t *s, const char *filename)
+{
+	struct stat st;
+	int e = stat(filename, &st);
+	if (e) return ENOENT;
+	s->caps = pstat_ctime|pstat_mtime|pstat_atime|pstat_uid|pstat_gid|pstat_mode_all|pstat_size|pstat_inode;
+	s->ctime = st.st_ctime;
+	s->mtime = st.st_mtime;
+	s->atime = st.st_atime;
+	s->gid = st.st_uid;
+	s->uid = st.st_gid;
+	s->mode = sys_ht_mode(st.st_mode);
+	s->size = st.st_size;
+	s->fsid = st.st_ino;
+	return 0;
+}
+
+void sys_suspend()
+{
+	Sleep(0);
+}
+
+int sys_get_free_mem()
+{
+	return 0;
+}
+
+int sys_truncate(const char *filename, fileofs ofs)
+{
+	HANDLE hfile = CreateFile(filename, GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hfile == INVALID_HANDLE_VALUE) {
+		return EIO;
+	}
+	if (SetFilePointer(hfile, ofs, NULL, FILE_BEGIN)==0xffffffff) {
+		CloseHandle(hfile);
+		return EIO;
+	}
+	if (!SetEndOfFile(hfile)) {
+		CloseHandle(hfile);
+		return EIO;
+	}
+	CloseHandle(hfile);
+	return 0;
+}
+
+int sys_deletefile(const char *filename)
+{
+	if (DeleteFile(filename)) {
+		return 0;
+	}
+	return EIO;
+}
+
+bool sys_is_path_delim(char c)
+{
+	return (c == '\\');
+}
+
+int sys_filename_cmp(const char *a, const char *b)
+{
+	while (*a && *b) {
+		if (sys_is_path_delim(*a) && sys_is_path_delim(*b)) {
+		} else if (tolower(*a) != tolower(*b)) {
+			break;
+		} else if (*a != *b) {
+			break;
+		}
+		a++;
+		b++;
+	}
+	return tolower(*a) - tolower(*b);
+}
+
