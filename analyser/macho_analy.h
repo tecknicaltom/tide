b5dac0801a2fb50d095e46c275d93a357248dbf2
seppel <seppel>
2005-07-07 16:40:00 +0000

    .

--- analyser/macho_analy.h
+++ analyser/macho_analy.h
@@ -56,7 +56,7 @@ public:
 	virtual	int		queryConfig(int mode);
 	virtual	bool 		validAddress(Address *Addr, tsectype action);
 	virtual	Address		*fileofsToAddress(FILEOFS fileofs);
-	virtual	FILEOFS		addressToFileofs(Address *Addr);
+	virtual	FileOfs		addressToFileofs(Address *Addr);
 	virtual	const char	*getSegmentNameByAddress(Address *Addr);
 };
 
