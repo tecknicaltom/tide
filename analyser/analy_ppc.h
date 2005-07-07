65892da3a0f572edfcedecd930c2dee9976e2fdd
seppel <seppel>
2005-07-07 20:41:47 +0000

    .

--- analyser/analy_ppc.h
+++ analyser/analy_ppc.h
@@ -26,14 +26,14 @@
 
 class AnalyPPCDisassembler: public AnalyDisassembler {
 public:
-			void			init(Analyser *A);
-	virtual   void	     	done();
+		void			init(Analyser *A);
+	virtual void			done();
 	virtual	ObjectID		getObjectID() const;
 
-	virtual	Address		*branchAddr(OPCODE *opcode, branch_enum_t branchtype, bool examine);
-			Address		*createAddress(uint32 offset);
+	virtual	Address			*branchAddr(OPCODE *opcode, branch_enum_t branchtype, bool examine);
+		Address			*createAddress(uint32 offset);
 	virtual	void			examineOpcode(OPCODE *opcode);
-	virtual	branch_enum_t 	isBranch(OPCODE *opcode);
+	virtual	branch_enum_t 		isBranch(OPCODE *opcode);
 };
 
 #endif
