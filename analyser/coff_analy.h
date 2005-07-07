b20fdb0cac8f80ec38bb563bbd3e2adc08b1cfb9
seppel <seppel>
2005-07-07 14:04:10 +0000

    .

---
+++ analyser/coff_analy.h
@@ -0,0 +1,59 @@
+/* 
+ *	HT Editor
+ *	coff_analy.h
+ *
+ *	Copyright (C) 1999-2002 Sebastian Biallas (sb@biallas.net)
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License version 2 as
+ *	published by the Free Software Foundation.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef coff_analy_h
+#define coff_analy_h
+
+#include "analy.h"
+#include "htcoff.h"
+
+
+class CoffAnalyser: public Analyser {
+public:
+	ht_coff_shared_data 	*coff_shared;
+	ht_streamfile 		*file;
+	Area			*validarea;
+
+		void		init(ht_coff_shared_data *Coff_shared, ht_streamfile *File);
+		int 		load(ObjectStream &f);
+	virtual	void		done();
+	virtual	ObjectID	getObjectID() const;
+
+	virtual	void		beginAnalysis();
+	virtual	uint		bufPtr(Address *Addr, byte *buf, int size);
+		bool		convertAddressToRVA(Address *addr, RVA *r);
+	virtual	Address		*createAddress();
+		Address		*createAddress32(uint32 addr);
+	virtual Assembler 	*createAssembler();
+	virtual	const char	*getName();
+	virtual	const char	*getSegmentNameByAddress(Address *Addr);
+	virtual   const char	*getType();
+	virtual	void 		initCodeAnalyser();
+	virtual	void 		initUnasm();
+	virtual	void 		log(const char *msg);
+	virtual	Address		*nextValid(Address *Addr);
+	virtual	void		store(ObjectStream &f);
+	virtual	int		queryConfig(int mode);
+	virtual	Address		*fileofsToAddress(FILEOFS fileofs);
+	virtual	FILEOFS		addressToFileofs(Address *Addr);
+	virtual	bool 		validAddress(Address *Addr, tsectype action);
+};
+
+#endif
