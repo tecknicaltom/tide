dd69886f38cb945243e00221f82c4a7588999384
seppel <seppel>
2005-07-07 20:41:47 +0000

    .

--- analyser/pef_analy.h
+++ analyser/pef_analy.h
@@ -33,8 +33,8 @@ public:
 	File		*file;
 	Area			*validarea;
 
-			void	init(ht_pef_shared_data *pef_shared, File *File);
-			int 	load(ObjectStream &f);
+		void		init(ht_pef_shared_data *pef_shared, File *File);
+		int 		load(ObjectStream &f);
 	virtual	void		done();
 	virtual	ObjectID	getObjectID() const;
 
@@ -45,14 +45,14 @@ public:
 		Address		*createAddress32(uint32 addr);
 		Address		*createAddress64(uint64 addr);
 	virtual	Assembler 	*createAssembler();
-	virtual	const char	*getName();
+	virtual	String &	getName(String &res);
 	virtual	const char	*getType();
 	virtual	void 		initCodeAnalyser();
 		void		initInsertSymbols(int shidx);
 	virtual	void 		initUnasm();
 	virtual	void 		log(const char *msg);
 	virtual	Address		*nextValid(Address *Addr);
-	virtual	void		store(ObjectStream &f);
+	virtual	void		store(ObjectStream &f) const;
 	virtual	int		queryConfig(int mode);
 	virtual	bool 		validAddress(Address *Addr, tsectype action);
 	virtual	Address		*fileofsToAddress(FileOfs fileofs);
