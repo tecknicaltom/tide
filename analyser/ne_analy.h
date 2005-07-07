0f8c9923b9ca49f48116e1f64feb0f1ed81915f1
seppel <seppel>
2005-07-07 20:41:47 +0000

    .

--- analyser/ne_analy.h
+++ analyser/ne_analy.h
@@ -31,7 +31,7 @@ public:
 	Area			*validarea;
 
 		void		init(ht_ne_shared_data *ne_shared, File *file);
-		int 		load(ObjectStream &f);
+		void 		load(ObjectStream &f);
 	virtual	void		done();
 	virtual	ObjectID	getObjectID() const;
 
@@ -41,13 +41,13 @@ public:
 	virtual	Address		*createAddress();
 		Address		*createAddress1616(uint16 seg, uint16 ofs);
 	virtual Assembler 	*createAssembler();
-	virtual	const char	*getName();
+	virtual	String &	getName(String &res);
 	virtual const char	*getType();
 	virtual	void 		initCodeAnalyser();
 	virtual	void 		initUnasm();
 	virtual	void 		log(const char *msg);
 	virtual	Address		*nextValid(Address *Addr);
-	virtual	void		store(ObjectStream &f);
+	virtual	void		store(ObjectStream &f) const;
 	virtual	int		queryConfig(int mode);
 	virtual	Address		*fileofsToAddress(FileOfs fileofs);
 	virtual	const char	*getSegmentNameByAddress(Address *Addr);
