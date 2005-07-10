347a0fdf04f2db97c458f441dd6d52b4cf11aef8
seppel <seppel>
2005-07-10 22:00:28 +0000

    .

--- analyser/flt_analy.h
+++ analyser/flt_analy.h
@@ -33,6 +33,8 @@ public:
 	File			*file;
 	Area			*validarea;
 
+				FLTAnalyser();
+				FLTAnalyser(BuildCtorArg&);
 		void		init(ht_flt_shared_data *flt_shared, File *File);
 		void 		load(ObjectStream &f);
 	virtual	void		done();
