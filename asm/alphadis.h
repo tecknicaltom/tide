74991ef31bd1189c929198f2e8e1914e739c947b
seppel <seppel>
2005-07-14 21:59:28 +0000

    .

--- asm/alphadis.h
+++ asm/alphadis.h
@@ -45,15 +45,13 @@ protected:
 	alphadis_insn	insn;
 public:
 				Alphadis();
-	virtual			~Alphadis();
+				Alphadis(BuildCtorArg&);
 
-		void 		load(ObjectStream &f);
 	virtual	dis_insn	*decode(byte *code, int maxlen, CPU_ADDR addr);
 	virtual	dis_insn	*duplicateInsn(dis_insn *disasm_insn);
 	virtual	void		getOpcodeMetrics(int &min_length, int &max_length, int &min_look_ahead, int &avg_look_ahead, int &addr_align);
 	virtual	byte		getSize(dis_insn *disasm_insn);
 	virtual	char		*getName();
-	virtual	void		store(ObjectStream &f) const;
 	virtual	char		*str(dis_insn *disasm_insn, int style);
 	virtual	char		*strf(dis_insn *disasm_insn, int style, char *format);
 	virtual	ObjectID	getObjectID() const;
