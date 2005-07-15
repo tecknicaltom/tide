4594b55fd518eccb26f82d37cd8874636af8434a
seppel <seppel>
2005-07-15 14:03:33 +0000

    .

--- output/out_txt.h
+++ output/out_txt.h
@@ -22,21 +22,20 @@
 #define OUT_TXT_H
 
 #include "analy.h"
-#include "common.h"
 #include "io/types.h"
 #include "stream.h"
 #include "out.h"
 
 class AnalyserTxtOutput: public AnalyserOutput {
-		ht_stream *stream;
+		Stream *stream;
 		char tmpbuf[1024];
 		int last;
 public:
-				void	init(Analyser *analy, ht_stream *stream);
+				void	init(Analyser *analy, Stream *stream);
 		virtual	void done();
 		virtual	void	beginAddr();
 		virtual	void	beginLine();
-		virtual	ht_stream *getGenerateStream();
+		virtual	Stream *getGenerateStream();
 		virtual	int	elementLength(const char *s);
 		virtual	void	endAddr();
 		virtual	void	endLine();
@@ -44,7 +43,7 @@ public:
 		virtual	void	header();
 		virtual	void putElement(int element_type, const char *element);
 		virtual	char *link(char *s, Address *Addr);
-		virtual	char *externalLink(char *s, int type1, int type2, int type3, int type4, void *special);
+		virtual	char *externalLink(char *s, uint32 type1, uint32 type2, uint32 type3, uint32 type4, void *special);
 };
 
 #endif
