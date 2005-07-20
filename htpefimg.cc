4c50d380410789e973793d18fabfcef7ae35bfbe
seppel <seppel>
2005-07-20 14:19:31 +0000

    .

--- htpefimg.cc
+++ htpefimg.cc
@@ -27,7 +27,7 @@
 #include "pefstruc.h"
 #include "snprintf.h"
 
-ht_view *htpefimage_init(bounds *b, File *file, ht_format_group *group)
+ht_view *htpefimage_init(Bounds *b, File *file, ht_format_group *group)
 {
 	ht_pef_shared_data *pef_shared=(ht_pef_shared_data *)group->get_shared_data();
 
@@ -35,7 +35,7 @@ ht_view *htpefimage_init(bounds *b, File *file, ht_format_group *group)
 	PEFAnalyser *p = new PEFAnalyser();
 	p->init(pef_shared, file);
 
-	bounds c=*b;
+	Bounds c=*b;
 	ht_group *g=new ht_group();
 	g->init(&c, VO_RESIZE, DESC_PEF_IMAGE"-g");
 	AnalyInfoline *head;
@@ -177,7 +177,7 @@ static int pe_viewer_func_section(eval_scalar *result, eval_scalar *q)
 /*
  *	CLASS ht_pef_aviewer
  */
-void ht_pef_aviewer::init(bounds *b, char *desc, int caps, File *File, ht_format_group *format_group, Analyser *Analy, ht_pef_shared_data *PEF_shared)
+void ht_pef_aviewer::init(Bounds *b, char *desc, int caps, File *File, ht_format_group *format_group, Analyser *Analy, ht_pef_shared_data *PEF_shared)
 {
 	ht_aviewer::init(b, desc, caps, File, format_group, Analy);
 	pef_shared = PEF_shared;
