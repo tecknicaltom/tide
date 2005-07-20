9598248a2879e19fe500c5daa5e38192cc9d35eb
seppel <seppel>
2005-07-20 14:19:32 +0000

    .

--- terminal.h
+++ terminal.h
@@ -56,7 +56,7 @@ private:
 			void do_update();
 			void get_pindicator_str(char *buf);
 public:
-			void init(bounds *b, Terminal *term, bool own_term);
+			void init(Bounds *b, Terminal *term, bool own_term);
 	virtual	void done();
 /* overwritten */
 	virtual	void handlemsg(htmsg *msg);
