d4302e58bb367224e51b0df6b937db756f10c9d5
seppel <seppel>
2005-07-20 14:19:31 +0000

    .

--- htneimg.h
+++ htneimg.h
@@ -36,7 +36,7 @@ public:
 	ht_ne_shared_data *ne_shared;
 	File *file;
 	
-		   void init(bounds *b, char *desc, int caps, File *file, ht_format_group *format_group, Analyser *Analyser, ht_ne_shared_data *ne_shared);
+		   void init(Bounds *b, char *desc, int caps, File *file, ht_format_group *format_group, Analyser *Analyser, ht_ne_shared_data *ne_shared);
 /* overwritten */
 	virtual char *func(uint i, bool execute);
 	virtual void setAnalyser(Analyser *a);
