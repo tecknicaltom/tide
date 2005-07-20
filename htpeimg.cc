d24dd860e9e045c4b1707a9719fc708dc7b95afc
seppel <seppel>
2005-07-20 14:19:32 +0000

    .

--- htpeimg.cc
+++ htpeimg.cc
@@ -28,7 +28,7 @@
 #include "pestruct.h"
 #include "snprintf.h"
 
-static ht_view *htpeimage_init(bounds *b, File *file, ht_format_group *group)
+static ht_view *htpeimage_init(Bounds *b, File *file, ht_format_group *group)
 {
 	ht_pe_shared_data *pe_shared=(ht_pe_shared_data *)group->get_shared_data();
 
@@ -40,7 +40,7 @@ static ht_view *htpeimage_init(bounds *b, File *file, ht_format_group *group)
 	PEAnalyser *p = new PEAnalyser();
 	p->init(pe_shared, file);
 
-	bounds c=*b;
+	Bounds c=*b;
 	ht_group *g=new ht_group();
 	g->init(&c, VO_RESIZE, DESC_PE_IMAGE"-g");
 	AnalyInfoline *head;
@@ -190,7 +190,7 @@ static int pe_viewer_func_section(eval_scalar *result, eval_scalar *q)
 /*
  *	CLASS ht_pe_aviewer
  */
-void ht_pe_aviewer::init(bounds *b, char *desc, int caps, File *File, ht_format_group *format_group, Analyser *Analy, ht_pe_shared_data *PE_shared)
+void ht_pe_aviewer::init(Bounds *b, char *desc, int caps, File *File, ht_format_group *format_group, Analyser *Analy, ht_pe_shared_data *PE_shared)
 {
 	ht_aviewer::init(b, desc, caps, File, format_group, Analy);
 	pe_shared = PE_shared;
