3203edc5fb17ab77d1885b3487b15da7e364b7b6
seppel <seppel>
2005-07-20 14:19:14 +0000

    .

--- htclipboard.h
+++ htclipboard.h
@@ -54,7 +54,7 @@ protected:
 			void get_pindicator_str(char *buf);
 			void selection_changed();
 public:
-			void init(bounds *b, char *desc, int caps, ht_clipboard *clipboard, ht_format_group *format_group);
+			void init(Bounds *b, char *desc, int caps, ht_clipboard *clipboard, ht_format_group *format_group);
 /* overwritten */
 	virtual	void draw();
 	virtual 	void handlemsg(htmsg *msg);
