adedaa96f6b4ad3944cad871e7c06ee5cf5fd6cd
seppel <seppel>
2005-07-29 00:40:52 +0000

    .

--- htpeexp.cc
+++ htpeexp.cc
@@ -21,7 +21,7 @@
 #include "formats.h"
 #include "htanaly.h"
 #include "htctrl.h"
-#include "htdata.h"
+#include "data.h"
 #include "htdebug.h"
 #include "endianess.h"
 #include "htiobox.h"
@@ -56,20 +56,23 @@ static ht_view *htpeexports_init(Bounds *b, File *file, ht_format_group *group)
 
 	uint32 *efunct=NULL, *enamet=NULL;
 	uint16 *eordt=NULL;
-	ht_mem_file *efile;
 	File *origfile = file;
-	const char *filename = file->get_filename();
-	char *esectionbuf;
+	String filename;
+	char *esectionbuf = NULL;
 	char eline[256];
 	FileOfs ename_ofs;
 	char *ename;
 	bool *lord;
+	String s;
+	ConstMemMapFile *efile = NULL;
 
 	ht_group *g = NULL;
 	Bounds c;
 	ht_statictext *head;
 	ht_pe_export_viewer *v = NULL;
 	
+	file->getFilename(filename);
+
 /* get export directory offset */
 	/* 1. get export directory rva */
 	FileOfs eofs;
@@ -77,32 +80,30 @@ static ht_view *htpeexports_init(Bounds *b, File *file, ht_format_group *group)
 	uint32 esize=pe_shared->pe32.header_nt.directory[PE_DIRECTORY_ENTRY_EXPORT].size;
 	/* 2. transform it into an offset */
 	if (!pe_rva_to_ofs(&pe_shared->sections, erva, &eofs)) goto pe_read_error;
-	LOG("%s: PE: reading export directory at offset %08x, rva %08x, size %08x...", filename, eofs, erva, esize);
+	LOG("%y: PE: reading export directory at offset 0x%08qx, rva %08x, size %08x...", &filename, eofs, erva, esize);
 
 /* make a memfile out of this section */
-	efile=new ht_mem_file();
-	efile->init(eofs, esize, FAM_READ | FAM_WRITE);
 	esectionbuf=(char*)malloc(esize);
 	file->seek(eofs);
-	file->read(esectionbuf, esize);
-	efile->write(esectionbuf, esize);
-	file=efile;
-	free(esectionbuf);
+	file->readx(esectionbuf, esize);
+
+	efile = new ConstMemMapFile(esectionbuf, esize, eofs);
+	file = efile;
 /* read export directory header */
 	PE_EXPORT_DIRECTORY edir;
 	file->seek(eofs);
-	file->read(&edir, sizeof edir);
+	file->readx(&edir, sizeof edir);
 	createHostStruct(&edir, PE_EXPORT_DIRECTORY_struct, little_endian);
 /* get export filename */
 	if (!pe_rva_to_ofs(&pe_shared->sections, edir.name_address, &ename_ofs)) goto pe_read_error;
 	file->seek(ename_ofs);
-	ename=fgetstrz(file);
+	ename=fgetstrz(efile);
 /* read in function entrypoint table */
 	FileOfs efunct_ofs;
 	efunct=(uint32*)malloc(edir.function_count*sizeof *efunct);
 	if (!pe_rva_to_ofs(&pe_shared->sections, edir.function_table_address, &efunct_ofs)) goto pe_read_error;
 	file->seek(efunct_ofs);
-	file->read(efunct, edir.function_count*sizeof *efunct);
+	file->readx(efunct, edir.function_count*sizeof *efunct);
 	for (uint i=0; i<edir.function_count;i++) {
 		efunct[i] = createHostInt(efunct+i, sizeof *efunct, little_endian);
 	}
@@ -111,7 +112,7 @@ static ht_view *htpeexports_init(Bounds *b, File *file, ht_format_group *group)
 	enamet=(uint32*)malloc(edir.name_count*sizeof *enamet);
 	if (!pe_rva_to_ofs(&pe_shared->sections, edir.name_table_address, &enamet_ofs)) goto pe_read_error;
 	file->seek(enamet_ofs);
-	file->read(enamet, edir.name_count*sizeof *enamet);
+	file->readx(enamet, edir.name_count*sizeof *enamet);
 	for (uint i=0; i<edir.name_count; i++) {
 		enamet[i] = createHostInt(enamet+i, sizeof *enamet, little_endian);
 	}
@@ -120,7 +121,7 @@ static ht_view *htpeexports_init(Bounds *b, File *file, ht_format_group *group)
 	eordt=(uint16*)malloc(edir.name_count*sizeof *eordt);
 	if (!pe_rva_to_ofs(&pe_shared->sections, edir.ordinal_table_address, &eordt_ofs)) goto pe_read_error;
 	file->seek(eordt_ofs);
-	file->read(eordt, edir.name_count*sizeof *eordt);
+	file->readx(eordt, edir.name_count*sizeof *eordt);
 	for (uint i=0; i<edir.name_count; i++) {
 		eordt[i] = createHostInt(eordt+i, sizeof *eordt, little_endian);
 	}
@@ -150,11 +151,10 @@ static ht_view *htpeexports_init(Bounds *b, File *file, ht_format_group *group)
 		FileOfs en;
 		if (!pe_rva_to_ofs(&pe_shared->sections, *(enamet+i), &en)) goto pe_read_error;
 		file->seek(en);
-		char *s = fgetstrz(file);
+		getStringz(efile, s);
 
 		ht_pe_export_function *efd = new ht_pe_export_function(f, o+edir.ordinal_base, s);
 		pe_shared->exports.funcs->insert(efd);
-		free(s);
 	}
 
 // sdgfdg
@@ -179,7 +179,7 @@ static ht_view *htpeexports_init(Bounds *b, File *file, ht_format_group *group)
 	g->insert(v);
 //
 	for (uint i=0; i<pe_shared->exports.funcs->count(); i++) {
-		ht_pe_export_function *e = (ht_pe_export_function *)pe_shared->exports.funcs->get(i);
+		ht_pe_export_function *e = (ht_pe_export_function *)(*pe_shared->exports.funcs)[i];
 		char ord[32], addr[32];
 		ht_snprintf(ord, sizeof ord, "%04x", e->ordinal);
 		ht_snprintf(addr, sizeof addr, "%08x", e->address);
@@ -196,17 +196,16 @@ static ht_view *htpeexports_init(Bounds *b, File *file, ht_format_group *group)
 
 	if (ename) free(ename);
 
-	efile->done();
-	delete efile;
-
 	if (efunct) free(efunct);
 	if (enamet) free(enamet);
 	if (eordt) free(eordt);
 	pe_shared->v_exports=v;
+	delete efile;
+	if (esectionbuf) free(esectionbuf);
 	return g;
 pe_read_error:
 	delete_timer(h0);
-	errorbox("%s: PE export directory seems to be corrupted.", origfile->get_filename());
+	errorbox("%y: PE export directory seems to be corrupted.", &filename);
 	if (efunct) free(efunct);
 	if (enamet) free(enamet);
 	if (eordt) free(eordt);
@@ -218,6 +217,8 @@ pe_read_error:
 		v->done();
 		delete v;
 	}
+	delete efile;
+	if (esectionbuf) free(esectionbuf);
 	return NULL;
 }
 
@@ -306,7 +307,7 @@ bool ht_pe_export_viewer::select_entry(void *entry)
 
 	ht_pe_shared_data *pe_shared=(ht_pe_shared_data *)format_group->get_shared_data();
 
-	ht_pe_export_function *e = (ht_pe_export_function*)pe_shared->exports.funcs->get(i->id);
+	ht_pe_export_function *e = (ht_pe_export_function*)(*pe_shared->exports.funcs)[i->id];
 	if (!e) return true;
 	if (pe_shared->v_image) {
 		ht_aviewer *av = (ht_aviewer*)pe_shared->v_image;
@@ -315,7 +316,7 @@ bool ht_pe_export_viewer::select_entry(void *entry)
 		if (pe_shared->opt_magic == COFF_OPTMAGIC_PE32) {
 			addr = a->createAddress32(e->address+pe_shared->pe32.header_nt.image_base);
 		} else {
-			addr = a->createAddress64(to_qword(e->address)+pe_shared->pe64.header_nt.image_base);
+			addr = a->createAddress64(e->address+pe_shared->pe64.header_nt.image_base);
 		}
 		if (av->gotoAddress(addr, NULL)) {
 			app->focus(av);
@@ -340,7 +341,7 @@ ht_pe_export_function::ht_pe_export_function(RVA addr, uint ord)
 	byname = false;
 }
 
-ht_pe_export_function::ht_pe_export_function(RVA addr, uint ord, char *n)
+ht_pe_export_function::ht_pe_export_function(RVA addr, uint ord, const char *n)
 {
 	ordinal = ord;
 	name = ht_strdup(n);
@@ -350,5 +351,5 @@ ht_pe_export_function::ht_pe_export_function(RVA addr, uint ord, char *n)
 
 ht_pe_export_function::~ht_pe_export_function()
 {
-	if ((byname) && (name)) free(name);
+	if (byname && name) free(name);
 }
