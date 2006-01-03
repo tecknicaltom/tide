22e8c3925c322b0ddf9262103832f7730603bcdf
seppel <seppel>
2006-01-03 14:24:35 +0000

    .

--- qword.h
+++ qword.h
@@ -18,7 +18,7 @@
  *	along with this program; if not, write to the Free Software
  *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-
+asfasfasfasfasf
 #ifndef __QWORD_H__
 #define __QWORD_H__
 
