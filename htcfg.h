da522f4982d33e413cf3b55b84ab243390b8b520
seppel <seppel>
2006-01-03 22:06:42 +0000

    .

--- htcfg.h
+++ htcfg.h
@@ -86,7 +86,7 @@ typedef void (*load_fcfg_func)(ObjectStream &f, void *context);
 typedef void (*store_fcfg_func)(ObjectStream &f, void *context);
 
 loadstore_result save_fileconfig(const char *fileconfig_file, const char *magic, uint version, store_fcfg_func store_func, void *context);
-loadstore_result load_fileconfig(const char *fileconfig_file, const char *magic, uint version, load_fcfg_func load_func, void *context, int *error_info);
+loadstore_result load_fileconfig(const char *fileconfig_file, const char *magic, uint version, load_fcfg_func load_func, void *context, String &error_info);
 
 /*
  *	INIT
