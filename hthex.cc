17c46aab486211a15f876e9e1599a7367e553778
seppel <seppel>
2006-01-14 03:35:42 +0000

    .

--- hthex.cc
+++ hthex.cc
@@ -40,7 +40,7 @@ ht_view *hthex_init(Bounds *b, File *file, ht_format_group *group)
 	ht_hex_viewer *v = new ht_hex_viewer();
 	v->init(b, DESC_HEX, VC_EDIT | VC_GOTO | VC_SEARCH | VC_REPLACE | VC_RESIZE, file, group);
 
-	v->search_caps|=SEARCHMODE_BIN | SEARCHMODE_EVALSTR | SEARCHMODE_EXPR;
+	v->search_caps |= SEARCHMODE_BIN | SEARCHMODE_EVALSTR | SEARCHMODE_EXPR;
 
 	v->h = new ht_hex_file_sub();
 	v->h->init(file, 0x0, file->getSize(), 16, 0);
@@ -92,56 +92,56 @@ bool ht_hex_viewer::get_vscrollbar_pos(int *pstart, int *psize)
 void ht_hex_viewer::handlemsg(htmsg *msg)
 {
 	switch (msg->msg) {
-		case msg_filesize_changed:
-			htmsg m;
-			m.msg=msg_filesize_changed;
-			m.type=mt_broadcast;
-			sendsubmsg(&m);
+	case msg_filesize_changed:
+		htmsg m;
+		m.msg=msg_filesize_changed;
+		m.type=mt_broadcast;
+		sendsubmsg(&m);
 
-			uf_initialized=false;
-			complete_init();
+		uf_initialized=false;
+		complete_init();
 
-			dirtyview();
-			return;
-		case cmd_hex_entropy: {
-			FileOfs ofs;
-			if (get_current_offset(&ofs)) {
-				byte buf[64];
-				if (pread(ofs, buf, 64)==64) {
-					int e = calc_entropy2(buf, 64);
-					infobox("64-byte entropy at offset %q08x: %d %%", ofs, e);
-				}
+		dirtyview();
+		return;
+	case cmd_hex_entropy: {
+		FileOfs ofs;
+		if (get_current_offset(&ofs)) {
+			byte buf[64];
+			if (pread(ofs, buf, 64)==64) {
+				int e = calc_entropy2(buf, 64);
+				infobox("64-byte entropy at offset %q08x: %d %%", ofs, e);
 			}
-			clearmsg(msg);
-			return;
 		}
-		case cmd_hex_display_bytes: {
-			char result[256];
-			sprintf(result, "%d", h->get_line_length());
-			if (inputbox("Change display width", "~Line length (Bytes)", result, 256)) {
-				char *p;
-				int ll = strtoul(result, &p, 10);
-				if (ll > 0 && ll <= 32) {
-					h->set_line_length(ll);
-				} else {
-					errorbox("Line length must be > 0 and <= 32!");
-				}
+		clearmsg(msg);
+		return;
+	}
+	case cmd_hex_display_bytes: {
+		char result[256];
+		sprintf(result, "%d", h->get_line_length());
+		if (inputbox("Change display width", "~Line length (Bytes)", result, 256)) {
+			char *p;
+			int ll = strtoul(result, &p, 10);
+			if (ll > 0 && ll <= 32) {
+				h->set_line_length(ll);
+			} else {
+				errorbox("Line length must be > 0 and <= 32!");
 			}
-			clearmsg(msg);
-			return;
 		}
-		case msg_contextmenuquery: {
-			ht_static_context_menu *m = new ht_static_context_menu();
-			m->init("~Local-Hex");
-			m->insert_entry("~Block operations", "Ctrl+B", cmd_file_blockop, K_Control_B, 1);
-			m->insert_entry("~Replace", "Ctrl+E", cmd_file_replace, K_Control_E, 1);
-			m->insert_entry("~Entropy", "Ctrl+T", cmd_hex_entropy, K_Control_T, 1);
-			m->insert_entry("~Change display width...", "Ctrl+O", cmd_hex_display_bytes, K_Control_O, 1);
+		clearmsg(msg);
+		return;
+	}
+	case msg_contextmenuquery: {
+		ht_static_context_menu *m = new ht_static_context_menu();
+		m->init("~Local-Hex");
+		m->insert_entry("~Block operations", "Ctrl+B", cmd_file_blockop, K_Control_B, 1);
+		m->insert_entry("~Replace", "Ctrl+E", cmd_file_replace, K_Control_E, 1);
+		m->insert_entry("~Entropy", "Ctrl+T", cmd_hex_entropy, K_Control_T, 1);
+		m->insert_entry("~Change display width...", "Ctrl+O", cmd_hex_display_bytes, K_Control_O, 1);
 
-			msg->msg = msg_retval;
-			msg->data1.ptr = m;
-			return;
-		}
+		msg->msg = msg_retval;
+		msg->data1.ptr = m;
+		return;
+	}
 	}
 	ht_uformat_viewer::handlemsg(msg);
 }
