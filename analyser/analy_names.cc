73d791050bb56ea486227367bb2bd1912af1255a
seppel <seppel>
2006-09-14 18:54:48 +0000

    .

--- analyser/analy_names.cc
+++ analyser/analy_names.cc
@@ -50,25 +50,25 @@ char *export_func_name(const char *funcname, int ordinal)
 	return ht_strdup(res);
 }
 
-char *label_types[] = {"unknown", "function", "location ", "data"};
-char *label_types_short[] = {"?   ", "func", "loc ", "data"};
+const char *label_types[] = {"unknown", "function", "location ", "data"};
+const char *label_types_short[] = {"?   ", "func", "loc ", "data"};
 
-char *label_type(int lt)
+const char *label_type(int lt)
 {
 	assert(lt < 4);
 	return label_types[lt];
 }
 
-char *label_type_short(int lt)
+const char *label_type_short(int lt)
 {
 	assert(lt < 4);
 	return label_types_short[lt];
 }
 
-char	*xref_types[] = {"read", "write", "offset", "jump", "call", "ijump", "icall"};
-char	xref_types_short[] = "rwojcJC";
+const char *xref_types[] = {"read", "write", "offset", "jump", "call", "ijump", "icall"};
+const char *xref_types_short = "rwojcJC";
 
-char *xref_type(int xt)
+const char *xref_type(int xt)
 {
 	assert(xt < 7);
 	return xref_types[xt];
@@ -120,54 +120,5 @@ void make_valid_name(char *result, const char *str)
 		str++;
 	}
 	*result = 0;
-}
-
-/*
- *
- */
-char the_label_prefix_string[2]="l";
-
-char *addr_label()
-{
-	return the_label_prefix_string;
-}
-
-char *real_name(char *s)
-{
-	if (!s) return NULL;
-	switch (s[0]) {
-		case M_PREFIX_LABEL: {
-			return "label";
-			break;
-		}
-		case M_PREFIX_DUP:
-		case M_PREFIX_REF: {
-			return &s[1];
-			break;
-		}
-		default: {}
-	}
-	return s;
-}
-
-char *quote_string(char *s)
-{
-	char *s2 = ht_malloc(strlen(s)+2);
-	s2[0] = M_PREFIX_DUP;
-	strcpy(&s2[1], s);
-	return s2;
-}
-
-char *comment_lookup(int special)
-{
-	return "testa";
-}
-
-void init_analy_names()
-{
-}
-
-void done_analy_names()
-{
 }
 
