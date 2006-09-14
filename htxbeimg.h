aeeef6f5d475b30ce8bdd72936bfb64b70e6970a
seppel <seppel>
2006-09-14 20:31:20 +0000

    .

--- htxbeimg.h
+++ htxbeimg.h
@@ -34,7 +34,7 @@ class ht_xbe_aviewer: public ht_aviewer {
 public:
 	ht_xbe_shared_data *xbe_shared;
 
-		void init(Bounds *b, char *desc, int caps, File *file, ht_format_group *format_group, Analyser *Analyser, ht_xbe_shared_data *xbe_shared);
+		void init(Bounds *b, const char *desc, int caps, File *file, ht_format_group *format_group, Analyser *Analyser, ht_xbe_shared_data *xbe_shared);
 	virtual bool func_handler(eval_scalar *result, char *name, eval_scalarlist *params);
 	virtual void setAnalyser(Analyser *a);
 };
