b06d656ec8e7dc3c21346487dc9d59d0708a3681
seppel <seppel>
2007-02-27 23:03:40 +0000

    .

--- htdisasm.cc
+++ htdisasm.cc
@@ -443,7 +443,7 @@ bool ht_disasm_sub::getline(char *line, const LINE_ID line_id)
 		s = "db ?";
 		c = 0;
 	}
-	l += sprintf(l, "%08qx", ofs);
+	l += ht_snprintf(l, 17, "%08qx", ofs);
 	*l++=' ';
 	for (int i=0; i<15; i++) {
 		if (i<c) {
