7a6b224a621b2ab1d8e6d634cb63219c83a00e3b
seppel <seppel>
2007-04-26 12:10:39 +0000

    .

--- output/out.cc
+++ output/out.cc
@@ -519,7 +519,7 @@ int	AnalyserOutput::generateFile(Address *from, Address *to)
 	if (!out) return OUTPUT_GENERATE_ERR_INVAL;
 	header();
 	int line = 0;
-	int len;
+	int len = 0;
 	while (from->compareTo(to) <= 0) {
 		char buffer[1024];
 		if (getLineString(buffer, sizeof buffer, from, line)) {
@@ -528,7 +528,7 @@ int	AnalyserOutput::generateFile(Address *from, Address *to)
 			if (out->write(buffer, wr) != wr) return OUTPUT_GENERATE_ERR_STREAM;
 			
 			int tmplen;
-			if (getLineByteLength(len, from, line)) {
+			if (getLineByteLength(tmplen, from, line)) {
 				len += tmplen;
 			}
 			line++;
@@ -682,7 +682,7 @@ int	AnalyserOutput::prevLine(Address *&Addr, int &line, int n, Address *min)
 //	fprintf(stdout, "prev_line(%x, %d, %d)\n", *Addr, *line, n);
 //#undef DPRINTF2
 //#define DPRINTF2(msg...) {ht_snprintf(tbuf, 1024, msg); fprintf(stderr, "%s", tbuf);}
-	char tbuf[1024];
+//	char tbuf[1024];
 	DPRINTF2("prev_line(%y, %d, %d, %y)\n", Addr, line, n, min);
 
 	int res = 0;
